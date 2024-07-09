4d984370c280902e3e6f94b16074f831df3773b4
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-09 18:55:18 +0300

    A method that get the user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,21 @@ class BasicAuth(Auth):
 
         email, pwd = decoded_base64_authorization_header.split(':')
         return (email, pwd)
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """Getting user"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if users is None or len(users) <= 0:
+            return None
+        user = users[0]
+        if user.is_valid_password(user_pwd):
+            return user
+        return None

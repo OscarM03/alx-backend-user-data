c4e569dfa65e703c076a3f978136ea961e5773c8
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-11 19:08:37 +0300

    Adding a logout function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@
 from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
+from api.v1.views import app_views
+from flask import request, jsonify
 
 
 class SessionAuth(Auth):
@@ -38,3 +40,18 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
+
+    def destroy_session(self, request=None):
+        """Logout"""
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True

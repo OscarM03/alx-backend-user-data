2d408caafaa549495a77960172764ed7e9bd9b63
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 11:21:47 +0300

    Creating a method that adds a new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,8 +33,12 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adding user to the database"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
+

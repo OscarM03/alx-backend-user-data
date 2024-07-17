63944abc7b10487809ec8e439e97d05d6cf90dbf
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 10:50:23 +0300

    Creating a method that adds a new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,29 +9,31 @@ from user import Base
 
 
 class DB:
-	"""DB class"""
+    """DB class
+    """
 
-	def __init__(self) -> None:
-		"""Initialize a new DB instance"""
-		self._engine = create_engine("sqlite:///a.db", echo=True)
-		Base.metadata.drop_all(self._engine)
-		Base.metadata.create_all(self._engine)
-		self.__session = None
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-	@property
-	def _session(self) -> Session:
-		"""Memoized session object"""
-		if self.__session is None:
-			DBSession = sessionmaker(bind=self._engine)
-			self.__session = DBSession()
-		return self.__session
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
-	def add_user(email, hashed_password):
-		"""Adding user to the database"""
-		from user import User
-		new_user = User(email=email, hashed_password=hashed_password)
-		session = self._session()
-		session.add(new_user)
-		session.commit()
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adding user to the database"""
+        from user import User
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
 
-		return new_user
+        return new_user

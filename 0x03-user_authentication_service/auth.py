56b4b90bc953a1b00ceccd5c66e1ba613d06b83e
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 13:05:18 +0300

    A method that returns a salted hash of the input password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,7 +6,7 @@ import bcrypt
 
 def _hash_password(passowrd: str) -> bytes:
     """Convert the password to bytes"""
-    bytes_pwd = passowrd.encode('utf-8')
+    bytes_pwd = passowrd.encode("utf-8")
     hashed_pwd = bcrypt.hashpw(bytes_pwd, bcrypt.gensalt())
 
     return hashed_pwd

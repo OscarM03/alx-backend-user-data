ecc4399325cc1c287d3d371eb779fb8575dd7060
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 15:08:04 +0300

    Adding a method to valid the credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,12 @@ class Auth:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate password"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        bytes_pwd = password.encode("utf-8")
+        return bcrypt.checkpw(bytes_pwd, user.hashed_password)

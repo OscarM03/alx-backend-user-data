d1550621bf339065a21b06793110685d1b54fe4a
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 14:52:20 +0300

    Addding a users routr for registering new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """Flask App"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -10,6 +14,18 @@ app = Flask(__name__)
 def home():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users() -> str:
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

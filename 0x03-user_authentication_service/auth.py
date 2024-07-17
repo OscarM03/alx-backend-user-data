519a67b09b942eacc32452bf7663dd90680b41b4
OscarM03 <oscarmutwiri03@gmail.com>
2024-07-17 15:21:19 +0300

    Adding a private method to generate a uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_pwd
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """generates uuid"""
     return str(uuid.uuid4())
 
